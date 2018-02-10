c351ab45791b2ec423adf6ca72eee76d1422945c
yishuangxi <yishuangxi@youzan.com>
2018-02-10 19:23:17 +0800

    2、使用koa-router配置路由

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
 const Koa = require('koa');
+const Router = require('koa-router');
+
 const app = new Koa();
+const router = new Router();
 const port = 3000;
 
-app.use(function(ctx, next) {
-    ctx.body = 'hello word'
+router.get('/', function(ctx, next) {
+    ctx.body = 'index page';
 });
+
+router.get('/about', function(ctx, next) {
+    ctx.body = 'about page';
+});
+
+
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(port, function() {
     console.log(`server is listen on ${port}`);

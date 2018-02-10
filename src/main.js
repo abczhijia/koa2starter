942f171d494a43ef44fbad647045ebf9bae2a903
yishuangxi <yishuangxi@youzan.com>
2018-02-10 19:59:20 +0800

    6、使用koa-static做静态资源服务

--- src/main.js
+++ src/main.js
@@ -1,14 +1,23 @@
 const Koa = require('koa');
 const path = require('path');
 const views = require('koa-views');
+const static = require('koa-static');
 const router = require('./router');
 
 const app = new Koa();
 const port = 3000;
 
+//静态资源
+app.use(static(path.join(__dirname, 'static'), {
+    gzip: true, maxage:999999}
+));
+
+//模板
 app.use(views(path.join(__dirname, 'views'), {
     map: {html: 'nunjucks'}
 }));
+
+//路由
 app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(port, function() {
